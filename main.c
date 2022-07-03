980d750ac74eca05750c4ffdd81d3f0820eea8bf
charv <34090168+charlesharville@users.noreply.github.com>
2022-07-02 20:01:25 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Enter wage: $__\b\b");
+    /* TODO: 1) write fgets() function for input 
+     *       2) convert input as necessary (check ebw for methods) */
+    
+    return 0;
+
+}
