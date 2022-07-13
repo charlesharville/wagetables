3f4225fc85f4b126dfa1f0a4f0954fd32342152f
charv <34090168+charlesharville@users.noreply.github.com>
2022-07-13 10:22:26 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -71,12 +71,11 @@ int main(void) {
     
     /* TODO: create a more elegant/complex table printing function */
     /* TODO: improve table readability */
-    printf("print table here, using data from file:\n");
     printf(" =============================================================================================================\n");
-    printf(" ||--- WageTable for %.2f an hour --------------------------------------------------------------------------||\n",
+    printf(" ||--- WageTable for %.2f an hour --------------------------------------------------------------------------\n",
            wage_f);
-    printf(" ||   Hourly  | Daily    | Weekly    | Monthly     | Quarterly     | Yearly     |                           ||\n");
-    printf(" ||   $%.2f  | $%.2f   | $%.2f   | $%.2f    | $%.2f      | $%.2f  |                           ||\n",
+    printf(" ||   Hourly  | Daily    | Weekly    | Monthly     | Quarterly     | Yearly     |                           \n");
+    printf(" ||   $%.2f  | $%.2f   | $%.2f   | $%.2f    | $%.2f      | $%.2f  |                           \n",
            wage_f, daily_earnings, weekly_earnings, monthly_earnings, quarterly_earnings, yearly_earnings);
     printf(" =============================================================================================================\n");
     
