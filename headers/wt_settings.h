15622d4e9f1b420dd41d91986632c522736c8616
charv <34090168+charlesharville@users.noreply.github.com>
2022-07-27 19:47:04 -0400

    Create wt_settings.h

---
+++ headers/wt_settings.h
@@ -0,0 +1,10 @@
+#ifndef WT_SETTINGS_H
+#define WT_SETTINGS_H
+
+/* This header contains functions dealing with default programme settings, and handles
+ * user-defined changes to programme settings.
+ * Other settings (related to user preferences, etc.), are taken care in wt_user_settings.h
+ * User accounts data are handled in wt_user_accounts.h
+ * User sessions data are handled in wt_user_sessions.h */
+
+#endif
